7d4e3e45da838a273808f9ae084667bdf5c9e2e8
davidPimienta <fdcp94@gmail.com>
2025-06-09 20:36:37 -0500

    App_run

--- app.py
+++ app.py
@@ -21,7 +21,14 @@ app.config.suppress_callback_exceptions = True
 
 # Load data from csv
 def load_data():
-    # To do: Completar la función 
+
+    data = pd.read_csv('datos_energia.csv')
+    
+    data['time'] = pd.to_datetime(data['time'])
+    
+    data.set_index('time', inplace=True)
+    
+    return data 
     
 
 # Cargar datos
@@ -240,4 +247,4 @@ def update_output_div(date, hour, proy):
 
 # Run the server
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(debug=True)
